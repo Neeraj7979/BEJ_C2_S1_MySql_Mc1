fa448377bf3604d25cedec637ff7a5a388a73270
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-28 01:07:57 +0530

    Created Interface TrackService inside service packages

---
+++ src/main/java/com/niit/bej/c2_s1_MySql_Mc1/service/TrackService.java
@@ -0,0 +1,10 @@
+package com.niit.bej.c2_s1_MySql_Mc1.service;
+
+import com.niit.bej.c2_s1_MySql_Mc1.domain.Track;
+
+import java.util.List;
+
+public interface TrackService {
+    List<Track> getAllTrack();
+
+}
