a9542cbd4f6111b0e42b2f0d6adf2ba7507e3417
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-28 01:40:08 +0530

    Added dependency injection of TrackService class inside the TrackController class.

--- src/main/java/com/niit/bej/c2_s1_MySql_Mc1/controller/TrackController.java
+++ src/main/java/com/niit/bej/c2_s1_MySql_Mc1/controller/TrackController.java
@@ -1,7 +1,17 @@
 package com.niit.bej.c2_s1_MySql_Mc1.controller;
 
+import com.niit.bej.c2_s1_MySql_Mc1.service.TrackService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrackController {
+    private final TrackService trackService;
+
+    @Autowired
+    public TrackController(TrackService trackService) {
+        this.trackService = trackService;
+    }
+
+
 }
