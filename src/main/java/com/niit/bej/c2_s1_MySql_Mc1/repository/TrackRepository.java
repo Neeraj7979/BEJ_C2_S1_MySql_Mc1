9bdcd2ea1338cf9e205d850a53bf24accefc3770
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-28 01:05:54 +0530

    Extended track repository with CrudClass and added method to find track by artist

--- src/main/java/com/niit/bej/c2_s1_MySql_Mc1/repository/TrackRepository.java
+++ src/main/java/com/niit/bej/c2_s1_MySql_Mc1/repository/TrackRepository.java
@@ -1,4 +1,13 @@
 package com.niit.bej.c2_s1_MySql_Mc1.repository;
 
-public interface TrackRepository {
+import com.niit.bej.c2_s1_MySql_Mc1.domain.Track;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TrackRepository extends CrudRepository<Track, Integer> {
+    List<Track> findTrackByArtistName(String artistName);
+
 }
