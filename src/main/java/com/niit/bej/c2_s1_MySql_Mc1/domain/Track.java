9bbc71fa61b6c82450820d9c06e5db0ac437972e
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-28 00:56:26 +0530

    Created fields for the class Track and generated primary key

--- src/main/java/com/niit/bej/c2_s1_MySql_Mc1/domain/Track.java
+++ src/main/java/com/niit/bej/c2_s1_MySql_Mc1/domain/Track.java
@@ -1,4 +1,18 @@
 package com.niit.bej.c2_s1_MySql_Mc1.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Track {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int trackId;
+    private String trackName;
+    private String trackComments;
+    private String trackArtist;
+    private double trackRatings;
+
 }
