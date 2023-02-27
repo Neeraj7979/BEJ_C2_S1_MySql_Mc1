7f3e9713bf26adeddd2f9d4ffa6dfb80c3a67bbd
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-28 00:58:24 +0530

    Created getter and setter for the fields of the domain class Track

--- src/main/java/com/niit/bej/c2_s1_MySql_Mc1/domain/Track.java
+++ src/main/java/com/niit/bej/c2_s1_MySql_Mc1/domain/Track.java
@@ -25,4 +25,44 @@ public class Track {
 
     public Track() {
     }
+
+    public int getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(int trackId) {
+        this.trackId = trackId;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getTrackComments() {
+        return trackComments;
+    }
+
+    public void setTrackComments(String trackComments) {
+        this.trackComments = trackComments;
+    }
+
+    public String getTrackArtist() {
+        return trackArtist;
+    }
+
+    public void setTrackArtist(String trackArtist) {
+        this.trackArtist = trackArtist;
+    }
+
+    public double getTrackRatings() {
+        return trackRatings;
+    }
+
+    public void setTrackRatings(double trackRatings) {
+        this.trackRatings = trackRatings;
+    }
 }
