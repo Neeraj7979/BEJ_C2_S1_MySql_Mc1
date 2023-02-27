8877e71d07ef98b872adde20dd0b3799187904f8
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-28 00:48:35 +0530

    Initial commit

---
+++ src/main/java/com/niit/bej/c2_s1_MySql_Mc1/TrackApplication.java
@@ -0,0 +1,13 @@
+package com.niit.bej.c2_s1_MySql_Mc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niit/bej/c2_s1_MySql_Mc1/TrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.bej.c2_s1_MySql_Mc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
