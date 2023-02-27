eb64419cdb45c993fea9bf2a630b7602e097ee79
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-28 01:16:52 +0530

    Created class TrackServiceImpl which will implement the interface TrackService and generated all the override methods

---
+++ src/main/java/com/niit/bej/c2_s1_MySql_Mc1/service/TrackServiceImpl.java
@@ -0,0 +1,37 @@
+package com.niit.bej.c2_s1_MySql_Mc1.service;
+
+import com.niit.bej.c2_s1_MySql_Mc1.domain.Track;
+
+import java.util.List;
+
+public class TrackServiceImpl implements TrackService {
+    @Override
+    public List<Track> getAllTrack() {
+        return null;
+    }
+
+    @Override
+    public Track addNewTrack(Track track) {
+        return null;
+    }
+
+    @Override
+    public Track getTrackById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Track updateTrack(Track track, Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Track> fetchByArtistName(String artistName) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteATrack(Integer id) {
+        return false;
+    }
+}
