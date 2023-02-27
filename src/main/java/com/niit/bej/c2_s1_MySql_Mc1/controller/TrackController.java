0ab51672c295adab025e3616128df820465e7ce4
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-28 01:44:20 +0530

    Created Fetmapping method to get all the track

--- src/main/java/com/niit/bej/c2_s1_MySql_Mc1/controller/TrackController.java
+++ src/main/java/com/niit/bej/c2_s1_MySql_Mc1/controller/TrackController.java
@@ -1,8 +1,14 @@
 package com.niit.bej.c2_s1_MySql_Mc1.controller;
 
+import com.niit.bej.c2_s1_MySql_Mc1.domain.Track;
 import com.niit.bej.c2_s1_MySql_Mc1.service.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TrackController {
@@ -11,6 +17,15 @@ public class TrackController {
     @Autowired
     public TrackController(TrackService trackService) {
         this.trackService = trackService;
+    }
+
+    @GetMapping("/getTrack")
+    public ResponseEntity<?> getAllTrack() {
+        List<Track> trackList = trackService.getAllTrack();
+        if (trackList.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else
+            return new ResponseEntity<>(trackList, HttpStatus.FOUND);
     }
 
 
